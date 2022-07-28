1e0dee1d380a45b18d6f7e78db632ad7ad7fba1e
HARIHARAN4369 <108862738+HARIHARAN4369@users.noreply.github.com>
2022-07-28 20:53:42 +0530

    Add files via upload

---
+++ rest countries.js
@@ -0,0 +1,20 @@
+
+var request=new XMLHttpRequest();
+
+request.open("GET","https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json");
+
+request.send();
+
+request.onload=function()
+{
+    
+    var result=JSON.parse(request.response);
+    console.log(result);
+
+for(var i=0;i<result.length;i++){
+    console.log(result[i].name);
+    console.log(result[i].region);
+    console.log(result[i].subregion);
+    console.log(result[i].population);
+}
+}
